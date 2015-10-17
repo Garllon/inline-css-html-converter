b6520df0a7b70e39a2842fd6f707ac752064ae17
Bennet Palluthe <palluthe.bennet@gmail.com>
2015-10-17 12:37:12 +0200

    add new specs for api key errors

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module InlineCssHtmlConverter
   describe Worker do
-    let(:apikey) { '1' }
+    let(:apikey) { '1-us1' }
     let(:html) { '<html></html>' }
 
     context '#initialize' do
@@ -14,6 +14,16 @@ module InlineCssHtmlConverter
       it 'set html' do
         worker = described_class.new(apikey, html)
         expect(worker.instance_variable_get("@html")).to eq html
+      end
+
+      it 'raise an error if no apikey is given' do
+        expect { described_class.new(nil, html) }
+          .to raise_error(NoApiKeyGivenError)
+      end
+
+      it 'raise an error if the api key is invalid' do
+        expect { described_class.new('1', html) }
+          .to raise_error(InvalidApiKeyError)
       end
     end
 
