64bce04161dd5bd7d003a3a78027b2caa0573099
Bennet Palluthe <palluthe.bennet@gmail.com>
2015-10-17 13:26:55 +0200

    add missing simplecov require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'inline-css-html-converter'
 require "codeclimate-test-reporter"
+require 'simplecov'
 
 SimpleCov.start
 CodeClimate::TestReporter.start
