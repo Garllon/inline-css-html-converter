0a4ce0184c6a3b9075280e73a33be7c8d8674258
Bennet Palluthe <palluthe.bennet@gmail.com>
2015-10-17 13:22:12 +0200

    simple cov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'inline-css-html-converter'
 require "codeclimate-test-reporter"
 
+SimpleCov.start
 CodeClimate::TestReporter.start
